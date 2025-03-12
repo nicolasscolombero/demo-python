148eed5463c5d8470de642089f1bd0ca027303b4
nicolasscolombero <nicolascolombero@MacBook-Pro-de-Nicolas-9.local>
2025-03-12 17:47:41 -0300

    crack

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import moduloA
-
+#from paquete2.moduloB import B
+import paquete2.moduloB
 print("hola marcelo")
 
 print(f"__name__: {__name__}")
 
-a = moduloA.A()
-print(a.saludo())
+#a = moduloA.A()
+b = paquete2.moduloB.B()
 
-
-
+print(b.captura_mensaje())
